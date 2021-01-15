df9a21960f8eb7bf0b8a2ee74e21e07b627015cd
shivi-25 <55458845+shivi-25@users.noreply.github.com>
2021-01-15 12:29:57 +0530

    Create Kadane's Algorithm.cpp

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    int ans=INT_MIN, sum=0, j=-1;
+    for(int i=0;i<n;++i)
+    {
+        sum+=arr[i];
+        ans=max(ans,sum);
+        if(sum<0)
+            sum=0;
+    }
+    return ans;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
