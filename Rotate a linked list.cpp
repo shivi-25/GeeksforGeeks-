59ea1ffc3dfa2dd95fa00ca9fa86f7f37ff6d1ee
shivi-25 <55458845+shivi-25@users.noreply.github.com>
2021-01-23 18:41:31 +0530

    Create Rotate a linked list.cpp

---
+++ Rotate a linked list.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *rotate(struct Node *head, int k);
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        head = rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// This function should rotate list counter-clockwise
+// by k and return new head (if changed)
+
+Node* rotate(Node* head, int k)
+{
+    int i=1;
+    Node *ptr=head , *temp, *p;
+    while(i<k)
+    {
+        ptr=ptr->next;
+        i++;
+    }
+    if(k==0 || ptr->next==NULL)
+       return head;
+    temp=ptr->next;
+    ptr->next=NULL;
+    p=temp;
+    while(p->next!=NULL)
+        p=p->next;
+    p->next=head;
+    return temp;
+}
